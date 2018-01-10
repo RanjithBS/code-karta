012f3890493db169d1e7e31302673b9f9ff4cfa9
RanjithBS <31505872+RanjithBS@users.noreply.github.com>
2018-01-10 15:59:11 +0530

    Create alphabetORnot.c

---
+++ alphabetORnot.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+  int n,i=0;
+  char a[50];
+  scanf("%s",&a[i]);
+  n=strlen(a);
+  for(i=0;i<=n;i++)
+  {
+  if(a[i]=='a'||a[i]=='b'||a[i]=='c'||a[i]=='d'||a[i]=='e'||a[i]=='f'||a[i]=='g'||a[i]=='h'||a[i]=='i'||a[i]=='j'||a[i]=='k'||a[i]=='l'||a[i]=='m'||a[i]=='n'||a[i]=='o'||a[i]=='p'||a[i]=='q'||a[i]=='r'||a[i]=='s'||a[i]=='t'||a[i]=='u'||a[i]=='v'||a[i]=='w'||a[i]=='x'||a[i]=='y'||a[i]=='z' )
+  {
+  printf("Alphabet");
+  }
+  else
+  {
+  printf("NO");
+  }
+	return 0;
+}
+}
