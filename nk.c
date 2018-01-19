82ac3d921e3c9aaa0544922973dda532cc8a9d32
RanjithBS <31505872+RanjithBS@users.noreply.github.com>
2018-01-19 16:56:31 +0530

    Create nk.c

---
+++ nk.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main() 
+{
+int i,n,k;
+int b=0;
+int a[10];
+scanf("%d %d", &n, &k);
+for(i=1;i<=n;i++)
+{
+scanf("%d",a[i]);
+}
+for(i=1;i<=k;i++)
+{
+b=+a[i];
+}
+printf("%d",b);
+}
