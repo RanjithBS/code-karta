33e1260cd16e6f04ab5bc329617739d6ee239cad
RanjithBS <31505872+RanjithBS@users.noreply.github.com>
2018-01-21 09:51:49 +0530

    Update nk.c

--- nk.c
+++ nk.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
-int main() 
+void main() 
 {
-int i,n,k,j;
+int i,j,n,k;
 int b=0;
+int a[10];
 scanf("%d",&n);
 scanf("%d",&k);
 for(i=1;i<=n;i++)
 {
-printf("%d",i);
+scanf("%d",&a[i]);
 }
 for(j=1;j<=k;j++)
 {
 b+=j;
 }
-printf("\n%d",b);
-return 0;
+printf("%d",b);
 }
 
 
