088684e53e495c4507f86d90799d91eb332c65fd
RanjithBS <31505872+RanjithBS@users.noreply.github.com>
2018-01-31 17:00:54 +0530

    Create multiplesof  5.c

---
+++ multiplesof  5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,n1,n2,n3,n4,n5;
+printf("enter the number");
+scanf("%d",&a);
+n1=a*1;
+n2=a*2;
+n3=a*3;
+n4=a*4;
+n5=a*5;
+printf("the result is  %d\t\t%d\t%d\t%d\t%d",n1,n2,n3,n4,n5);
+return 0;
+}
